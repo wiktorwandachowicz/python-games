ee7fbef5d40210990b748cbaeab26076b319fd61
Krzysztof Wandachowicz <kwandachowicz@gmail.com>
2021-06-26 16:26:50 +0200

    Added checkage() function

--- age.py
+++ age.py
@@ -1,19 +1,25 @@
-age = 43
-print("Your age is " + str(age) + ", so...")
+age = 11
 
-if age == 11:
-	print("You are my age!")
-elif age < 0:
-	print("You are not alive.")
-elif age < 1:
-	print("You are BARELY alive!")
-elif age == 43:
-	print("You are as old as my mother!")
-elif age > 43 and age < 45:
-	print("You are older than my mother and younger than my dad!")
-elif age == 45:
-	print("You are as old as my dad!")
-elif age > 45:
-	print("You are older than my dad!")
-else:
-	print("You are NOT my age!")
+def checkage(age):
+    if age == 11:
+            return ("you are my age!")
+    elif age < 0:
+            return ("you are not alive.")
+    elif age < 1:
+            return ("you are BARELY alive!")
+    elif age == 43:
+            return ("you are as old as my mother!")
+    elif age > 43 and age < 45:
+            return ("you are older than my mother and younger than my dad!")
+    elif age == 45:
+            return ("you are as old as my dad!")
+    elif age > 45:
+            return ("you are older than my dad!")
+    else:
+            return ("you are NOT my age!")
+
+x = checkage(age)
+
+howifeelabouturage = "Your age is " + str(age) + ", so " + x
+
+print(howifeelabouturage)
